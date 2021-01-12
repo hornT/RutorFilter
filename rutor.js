65b2348644b24f55aaed4e648ca456a053ec4da3
Nikolay Neverov <neverov.nikolay@gmail.com>
2021-01-12 21:50:20 +0300

    update selector

--- rutor.js
+++ rutor.js
@@ -9,19 +9,21 @@ function filter(items){
 	console.log('keyWordsSummReg: %s', keyWordsSummReg);
 
 	// получить все новые
-	// (\(((19\d{2})|(200\d)|(201[01234]))\))|Online
-	// Сериалы|Музыка|Телевизор|Софт|Научно-популярные фильмы|Наши фильмы|Мультипликация|Спорт|Юмор
+	// (\(((19\d{2})|(200\d)|(201[0123456789]))\))|Online|Обои для рабочего стола|КПК|UKR|WEB-DL
+	// Сериалы|Музыка|Телевизор|Софт|Научно-популярные фильмы|Наши фильмы|Мультипликация|Спорт|Юмор|Зарубежные сериалы|Наши сериалы|Аниме
 
-	var trakers = document.querySelector(".list-entries").querySelectorAll(".unread");
+	var trakers = document.querySelector(".list-entries").querySelectorAll(".entry--unread");
 	var ln = trakers.length;
 	var sortMas = {};
 	console.log('ln: %s', ln);
 	for(var i = ln - 1; i >= 0; i--){
-		var title = trakers[i].attributes['data-title'].value;
+		const dataTitle = trakers[i].attributes['data-title'];
+		if(!dataTitle) continue;
+		
+		var title = dataTitle.value;
 		var summary = trakers[i].querySelector('.summary').innerHTML;
 		
 		var skip = keyWordsReg.test(title) || keyWordsSummReg.test(summary);
-		//console.log('i: %s, skip: %s, title: %s', i, skip, title);
 		
 		if(skip){
 			console.log('delete: %s', title);
